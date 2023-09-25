2a27a974a483a78b0deebc058e1806693d4f08ed
Lualmara-5 <luimartinezra@unal.edu.co>
2023-09-25 13:03:28 -0500

    Update animal.py

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -1,3 +1,34 @@
 class Animal:
 
     _totalAnimales = 0
+
+    def __init__(self, nombre, edad, habitat, genero):
+        self._nombre = nombre
+        self._edad = edad
+        self._habitat = habitat
+        self._genero = genero
+        Animal._totalAnimales += 1
+    
+    def setNombre(self, nombre):
+        self._nombre = nombre
+        
+    def getNombre(self):
+        return self._nombre
+    
+    def setEdad(self, edad):
+        self._edad = edad
+
+    def getEdad(self):
+        return self._edad
+    
+    def setHabitat(self, habitat):
+        self._habitat = habitat
+
+    def getHabitat(self):
+        return self._habitat
+    
+    def setGenero(self, genero):
+        self._genero = genero
+
+    def getGenero(self):
+        return self._genero

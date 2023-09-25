f73c85aa614f179aa835ba7ac1cf1ba103279c7d
Lualmara-5 <luimartinezra@unal.edu.co>
2023-09-25 14:17:04 -0500

    Update anfibio.py

--- zooAnimales/anfibio.py
+++ zooAnimales/anfibio.py
@@ -1,5 +1,41 @@
-class Anfibio:
+from zooAnimales.animal import Animal
+
+class Anfibio(Animal):
 
     _listado = []
     ranas = 0
     salamandras = 0
+
+    def __init__(self, nombre, edad, habitat, genero, colorPiel, venenoso):
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorPiel = colorPiel
+        self._venenoso = venenoso
+        Anfibio._listado.append(self)
+    
+    def setColorPiel(self, colorPiel):
+        self._colorPiel = colorPiel
+
+    def getColorPiel(self):
+        return self._colorPiel
+    
+    def setVenenoso(self, venenoso):
+        self._venenoso = venenoso
+
+    def isVenenoso(self):
+        return self._venenoso
+    
+    @classmethod
+    def crearRana(cls, nombre, edad, genero):
+        rana = cls(nombre, edad, "selva", genero, "rojo", True)
+        cls.ranas += 1
+        return rana
+
+    @classmethod
+    def crearSalamandra(cls, nombre, edad, genero):
+        salamandra = cls(nombre, edad, "selva", genero, "negro y amarillo", False)
+        cls.salamandras += 1
+        return salamandra
+    
+    @staticmethod
+    def cantidadAnfibios():
+        return len(Anfibio._listado)

9ca803a98847d46c905db749f9f7f3e01c50b91d
Lualmara-5 <luimartinezra@unal.edu.co>
2023-09-25 23:12:04 -0500

    Update pez.py

--- zooAnimales/pez.py
+++ zooAnimales/pez.py
@@ -1,5 +1,41 @@
-class Pez:
+from zooAnimales.animal import Animal
+
+class Pez(Animal):
 
     _listado = []
     salmones = 0
     bacalaos = 0
+
+    def __init__(self, nombre, edad, habitat, genero, colorEscamas, cantidadAletas):
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorEscamas = colorEscamas
+        self._cantidadAletas = cantidadAletas
+        Pez._listado.append(self)
+    
+    def setColorEscamas(self, colorEscamas):
+        self._colorEscamas = colorEscamas
+
+    def getColorEscamas(self):
+        return self._colorEscamas
+    
+    def setCantidadAletas(self, cantidadAletas):
+        self._cantidadAletas = cantidadAletas
+        
+    def getCantidadAletas(self):
+        return self._cantidadAletas
+    
+    @classmethod
+    def crearSalmon(cls, nombre, edad, genero):
+        salmon = cls(nombre, edad, "oceano", genero, "rojos", 6)
+        cls.salmones += 1
+        return salmon
+    
+    @classmethod
+    def crearBacalao(cls, nombre, edad, genero):
+        bacalao = cls(nombre, edad, "oceano", genero, "gris", 6)
+        cls.bacalaos += 1
+        return bacalao
+    
+    @staticmethod
+    def cantidadPeces():
+        return len(Pez._listado)
